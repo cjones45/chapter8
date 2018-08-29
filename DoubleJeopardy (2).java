dac8f7e3cf3b5151aa37800891cc79ea8c0837ee
Caleb Jones <c.jones5872@gmail.com>
2018-08-29 09:10:22 -0600

    upload

---
+++ DoubleJeopardy (2).java
@@ -0,0 +1,10 @@
+public class DoubleJeopardy
+{
+  public static void main ( String[] args )
+  {
+    double value = 32;
+    System.out.println("A double: " +  value);
+    System.out.println("Hold Tight my man");
+
+  }
+}
